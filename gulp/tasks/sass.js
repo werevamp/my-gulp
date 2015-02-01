39d54376efd77748179fafef5470d03e5c6c2e25
Albert De Guzman <albertdeguzma@gmail.com>
2015-02-01 00:38:21 -0800

    added error handling

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,8 +1,9 @@
-var gulp = require('gulp');
-var browser_sync = require('browser-sync');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp          = require('gulp');
+var browser_sync  = require('browser-sync');
+var sass          = require('gulp-sass');
+var sourcemaps    = require('gulp-sourcemaps');
+var autoprefixer  = require('gulp-autoprefixer');
+var handle_errors = require('../util/handle_errors');
 
 var paths {
 	scss: './scss/**/*.scss',
@@ -13,6 +14,7 @@ gulp.task('sass', function() {
 	return gulp.src(paths.scss)
 		.pipe(sourcemaps.init())
 		.pipe(sass())
+		.on('errors', handle_errors)
 		.pipe(sourcemaps.write())
 		.pipe(autoprefixer({ browsers: ['last 2 version'] }))
 		.pipe(gulp.dest(paths.css))
