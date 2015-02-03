eac7031e28fc8cff1e2ba29e28b61f2d79fe2888
Albert De Guzman <albertdeguzma@gmail.com>
2015-02-02 21:15:32 -0800

    Added a bundle logger file

---
+++ gulp/util/bundle-logger.js
@@ -0,0 +1,20 @@
+var gutil = require('gulp-util');
+var pretty_hrtime = require('pretty-hrtime');
+var start_time;
+
+module.export = {
+	start: function(filepath) {
+		start_time = process.hrtime();
+		gutil.log('Bundling', gutil.colors.green(filepath) + '...');
+	},
+
+	watch: function(bundle_name) {
+		gutil.log('Watching files required by', gutil.colors.yellow(bundle_name));
+	},
+
+	end: function(filepath) {
+		var task_time = process.hrtime(start_time);
+		var pretty_time = pretty_hrtime(task_time);
+		gutil.log('Bundled', gutil.colors.green(filepath), 'in', gutil.colors.magenta(pretty_time));
+	}
+};
